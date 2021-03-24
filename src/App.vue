231a2aa01469118607e6580c96ce6e76ca366c5e
Chris Waring <chris@wwaves.co>
2021-03-24 15:46:13 +0000

    base init()

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Origin logo" src="./assets/origin-logo.svg" />
+  <HelloWorld msg="origin-vue-ts" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Ubuntu, roboto, noto, arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 3rem;
+  line-height: 1.5em;
+}
+</style>
