2607c6c591c14a301f92e43eabe04aec93d032b7
cwaring <chris@wwaves.co>
2021-07-12 18:03:03 +0100

    refactor eslint config and add new plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,35 @@
 module.exports = {
   root: true,
-  parser: 'vue-eslint-parser',
   env: {
     browser: true,
     es2021: true,
     node: true
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
+  ],
   extends: [
     'standard',
     'plugin:vue/vue3-recommended',
+    'plugin:jsonc/recommended-with-jsonc',
+    'plugin:yml/standard',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended'
   ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
   rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off'
   }
