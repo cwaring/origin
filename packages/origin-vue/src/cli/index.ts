b81eea0da4da7b72a37f41eed5a18b8cc7f550e9
Chris Waring <106938+cwaring@users.noreply.github.com>
2021-09-10 15:29:40 +0100

    feat: add cli wrapper

---
+++ packages/origin-vue/src/cli/index.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import { spawn } from 'child_process'
+import { cac } from 'cac'
+
+/**
+ * Origin Cli vite wrapper for core app methods
+ * Allows control for rendering and build steps
+ */
+const cli = cac('origin')
+
+cli.command('build', 'Run production build').action((args) => {
+  spawn('vite-ssg', ['build'], {
+    stdio: 'inherit',
+    env: { NODE_ENV: 'production', ...process.env }
+  }).on('error', function (err) {
+    throw err
+  })
+})
+
+cli.command('dev', 'Run development mode and watch project').action((args) => {
+  spawn('vite', {
+    stdio: 'inherit',
+    env: { NODE_ENV: 'development', ...process.env }
+  }).on('error', function (err) {
+    throw err
+  })
+})
+
+cli
+  .command('preview', 'Serve compiled project output preview')
+  .action((args) => {
+    spawn('vite', ['preview'], {
+      stdio: 'inherit',
+      env: { NODE_ENV: 'pro', ...process.env }
+    }).on('error', function (err) {
+      throw err
+    })
+  })
+
+cli.help()
+cli.version(require('../../../package.json').version)
+cli.parse()
